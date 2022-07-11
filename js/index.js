4535a0d267031942394d868715897b840bbc9760
FayozbekOlimov <fayozbek2021dev@gmail.com>
2022-07-11 17:25:48 +0500

    one section added

---
+++ js/index.js
@@ -0,0 +1,59 @@
+// line graph
+
+const xValues = ['2021-yil yakuniga ko\'ra rejada (mlrd so\'m)', '2021-yil yakuniga ko\'ra amalda (mlrd so\'m)'];
+const yValues = [1400, 1470];
+
+new Chart("myLineGraph", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "#D3B1E0",
+      borderColor: "#8D35B0",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: { display: false },
+    scales: {
+      yAxes: [{ ticks: { min: 6, max: 16 } }],
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+  }
+});
+
+// pie chart
+
+const x = ["Yangi ish joylarini yaratish rejada", "Yangi ish joylarini yaratish amalda"];
+const y = [823, 824];
+const barColors = ["#80DC87", "#6F009B"];
+
+new Chart("myPieChart", {
+  type: "doughnut",
+  data: {
+    labels: x,
+    datasets: [{
+      backgroundColor: barColors,
+      data: y
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: "World Wide Wine Production 2018"
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+  }
+});
